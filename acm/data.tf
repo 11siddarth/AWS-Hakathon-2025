811c22833a506ef07e1790533247fd4cb4008037
11siddarth <92918528+11siddarth@users.noreply.github.com>
2025-09-24 11:54:12 +0530

    Delete acm directory

--- acm/data.tf
+++
@@ -1,6 +0,0 @@
-
-data "aws_route53_zone" "example" {
-  name = "devops4solutions.com"  # Replace with your domain
-}
-
-
--- acm/variables.tf
+++
@@ -1,6 +0,0 @@
-variable "environment" {
-  default = "dev"
-}
-variable "domain_name"{
-  default = "devops4solutions.com"
-}
